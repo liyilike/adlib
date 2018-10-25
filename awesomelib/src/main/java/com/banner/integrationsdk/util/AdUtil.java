6cafdd98535155bc6a940d000195e28ce09ad347
liyicool <liyiliyi.cool@gmail.com>
2018-10-25 10:24:10 +0800

    2
    2

--- awesomelib/src/main/java/com/banner/integrationsdk/util/AdUtil.java
+++ awesomelib/src/main/java/com/banner/integrationsdk/util/AdUtil.java
@@ -8,11 +8,6 @@ import android.view.ViewGroup;
 
 import com.banner.integrationsdk.Interface.ADCallBackInterface;
 import com.banner.integrationsdk.R;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.android.gms.ads.MobileAds;
 import com.qq.e.ads.banner.ADSize;
 import com.qq.e.ads.banner.AbstractBannerADListener;
 import com.qq.e.ads.banner.BannerView;
@@ -28,16 +23,16 @@ public class AdUtil {
     private static AdUtil Instance = null;
     private static Activity mContext;
     private static InterstitialAD iad;
-    private static InterstitialAd interstitialAd;
+    //    private static InterstitialAd interstitialAd;
     private static SplashAD splashAD;
-    private static String qqID = null, qq_IAD_KEY = null, qq_BAN_KEY = null, qq_SP_KEY = null, admob_BAN = null, admob_IAD = null;
+    private static String qqID = null, qq_IAD_KEY = null, qq_BAN_KEY = null, qq_SP_KEY = null;
+//            , admob_BAN = null, admob_IAD = null;
 
     public static AdUtil getInstance(Activity Context) {
         mContext = Context;
         if (Instance == null) {
             Instance = new AdUtil();
             init();
-
             return Instance;
         }
         return Instance;
@@ -48,39 +43,39 @@ public class AdUtil {
         qq_IAD_KEY = mContext.getString(R.string.qq_iad_key);
         qq_BAN_KEY = mContext.getString(R.string.qq_ban_key);
         qq_SP_KEY = mContext.getString(R.string.qq_sp_key);
-        admob_IAD = mContext.getString(R.string.admob_iad);
-        admob_BAN = mContext.getString(R.string.admob_ban);
+//        admob_IAD = mContext.getString(R.string.admob_iad);
+//        admob_BAN = mContext.getString(R.string.admob_ban);
         //初始化谷歌广告的id
-        MobileAds.initialize(mContext, mContext.getString(R.string.admob_id));
+//        MobileAds.initialize(mContext, mContext.getString(R.string.admob_id));
     }
 
 
     public void showAD() {
         Locale locale = mContext.getResources().getConfiguration().locale;
         String language = locale.getLanguage();
-        if (!language.equals("zh")) {
-            InterstitialGoogleAD(new ADCallBackInterface() {
-                @Override
-                public void onADReceive() {
-
-                }
-
-                @Override
-                public void onNoAD() {
-                    InterstitialAD(null);
-                }
-
-                @Override
-                public void onADClicked() {
-
-                }
-
-                @Override
-                public void onADClosed() {
-                }
-            });
-            return;
-        }
+//        if (!language.equals("zh")) {
+////            InterstitialGoogleAD(new ADCallBackInterface() {
+////                @Override
+////                public void onADReceive() {
+////
+////                }
+////
+////                @Override
+////                public void onNoAD() {
+////                    InterstitialAD(null);
+////                }
+////
+////                @Override
+////                public void onADClicked() {
+////
+////                }
+////
+////                @Override
+////                public void onADClosed() {
+////                }
+////            });
+//            return;
+//        }
 
 
         InterstitialAD(new ADCallBackInterface() {
@@ -91,7 +86,7 @@ public class AdUtil {
 
             @Override
             public void onNoAD() {
-                InterstitialGoogleAD(null);
+//                InterstitialGoogleAD(null);
             }
 
             @Override
@@ -129,18 +124,18 @@ public class AdUtil {
         bv.loadAD();
     }
 
-    public static void AdmobBan(AdView mAdView) {
-        MobileAds.initialize(mContext.getApplicationContext(), admob_BAN);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-    }
+//    public static void AdmobBan(AdView mAdView) {
+//        MobileAds.initialize(mContext.getApplicationContext(), admob_BAN);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        mAdView.loadAd(adRequest);
+//    }
 
     public void InterstitialAD(final ADCallBackInterface adCallBackInterface) {
 
         if (ContextCompat.checkSelfPermission(mContext, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 | ContextCompat.checkSelfPermission(mContext, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            AdUtil.getInstance(mContext).InterstitialGoogleAD(null);
-            return;
+//            AdUtil.getInstance(mContext).InterstitialGoogleAD(null);
+//            return;
         }
 
         if (iad == null) {
@@ -183,44 +178,44 @@ public class AdUtil {
     }
 
 
-    public static void InterstitialGoogleAD(final ADCallBackInterface adCallBackInterface) {
-        interstitialAd = new InterstitialAd(mContext);
-        AdRequest adRequest = new AdRequest.Builder().setRequestAgent("android_studio:ad_template").build();
-        interstitialAd.setAdUnitId(admob_IAD);
-        interstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                if (adCallBackInterface != null) {
-                    adCallBackInterface.onADReceive();
-                }
-                interstitialAd.show();
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                if (adCallBackInterface != null) {
-                    adCallBackInterface.onNoAD();
-                }
-            }
-
-            @Override
-            public void onAdClosed() {
-                if (adCallBackInterface != null) {
-                    adCallBackInterface.onADClosed();
-                }
-
-            }
-
-            @Override
-            public void onAdClicked() {
-                super.onAdClicked();
-                if (adCallBackInterface != null) {
-                    adCallBackInterface.onADClicked();
-                }
-            }
-        });
-        interstitialAd.loadAd(adRequest);
-    }
+//    public static void InterstitialGoogleAD(final ADCallBackInterface adCallBackInterface) {
+//        interstitialAd = new InterstitialAd(mContext);
+//        AdRequest adRequest = new AdRequest.Builder().setRequestAgent("android_studio:ad_template").build();
+//        interstitialAd.setAdUnitId(admob_IAD);
+//        interstitialAd.setAdListener(new AdListener() {
+//            @Override
+//            public void onAdLoaded() {
+//                if (adCallBackInterface != null) {
+//                    adCallBackInterface.onADReceive();
+//                }
+//                interstitialAd.show();
+//            }
+//
+//            @Override
+//            public void onAdFailedToLoad(int errorCode) {
+//                if (adCallBackInterface != null) {
+//                    adCallBackInterface.onNoAD();
+//                }
+//            }
+//
+//            @Override
+//            public void onAdClosed() {
+//                if (adCallBackInterface != null) {
+//                    adCallBackInterface.onADClosed();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onAdClicked() {
+//                super.onAdClicked();
+//                if (adCallBackInterface != null) {
+//                    adCallBackInterface.onADClicked();
+//                }
+//            }
+//        });
+//        interstitialAd.loadAd(adRequest);
+//    }
 
     public static void SplashAD(ViewGroup adContainer, View skipContainer, int fetchDelay, final ADCallBackInterface adCallBackInterface) {
         splashAD = new SplashAD(mContext, adContainer, skipContainer, qqID, qq_SP_KEY, new SplashADListener() {
